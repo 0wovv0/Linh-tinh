25be237cc89d3285be64ed6406220c35f5fa5d93
Pham Viet Hung <149321460+0wovv0@users.noreply.github.com>
2025-05-06 18:05:38 +0700

    Up thử file test

---
+++ test.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const div = document.createElement("div");
+    div.textContent = "Hello World";
+    div.style.fontSize = "24px";
+    div.style.color = "blue";
+    div.style.textAlign = "center";
+    div.style.marginTop = "20px";
+    document.body.appendChild(div);
+});
