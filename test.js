b9ebd7c7f04aae2f2bfa0012cec4f3682960c587
Pham Viet Hung <Hoktap.work@gmail.com>
2025-07-25 22:37:59 +0700

    N8N
    Ừm Upload cái n8n là chính nhé ✌️

--- test.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const div = document.createElement("div");
-    div.textContent = "Hello World";
-    div.style.fontSize = "24px";
-    div.style.color = "blue";
-    div.style.textAlign = "center";
-    div.style.marginTop = "20px";
-    document.body.appendChild(div);
-});
